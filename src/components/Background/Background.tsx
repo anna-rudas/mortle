26809473f878a4458db1139f255c0a7b2a67a4bd
Anna Rudas <rudas.annamaria@gmail.com>
2023-01-05 15:12:08 +0100

    add background hills

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Cloud from "../Cloud/Cloud";
+import Hills from "../Hills/Hills";
 
 function Background() {
   return (
@@ -7,6 +8,7 @@ function Background() {
       <div className="sun"></div>
       <Cloud cloudClass="small-cloud" />
       <Cloud cloudClass="large-cloud" />
+      <Hills />
     </div>
   );
 }
