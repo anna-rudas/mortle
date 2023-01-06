26713afff0198ac15a132ed3ce107b063baac110
Anna Rudas <rudas.annamaria@gmail.com>
2023-01-06 16:18:54 +0100

    add Game component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Background from "./components/Background/Background";
+import Game from "./components/Game/Game";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="wrapper">
       <Background />
-      <Header />
+      <div className="game-content">
+        <Header />
+        <Game />
+      </div>
     </div>
   );
 }
