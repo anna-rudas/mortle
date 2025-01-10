a636d84da7c1b1f31f4aec269b3284e759d751fb
Anna Rudas <rudas.annamaria@gmail.com>
2025-01-10 13:43:24 +0100

    add test for handling letter recoloring in MainPage tests

--- src/__tests__/MainPage.test.tsx
+++ src/__tests__/MainPage.test.tsx
@@ -29,6 +29,161 @@ describe("colors letters correctly (solution: CHANT)", () => {
 
   jest.setTimeout(10000);
 
+  test("handles recoloring letters correctly", async () => {
+    const { getAllByTestId, getByTestId } = render(
+      <AppContextProvider>
+        <MainPage />
+      </AppContextProvider>
+    );
+
+    await waitFor(() => {
+      const loadingText = screen.queryByText("Loading");
+      //check that app is finished loading
+      expect(loadingText).toBeNull();
+    });
+
+    const firstRowContainers = getAllByTestId("row-0");
+    const firstRowInputs = firstRowContainers.map((rowItem) => {
+      return within(rowItem).getByRole("textbox");
+    });
+
+    const secondRowContainers = getAllByTestId("row-1");
+    const secondRowInputs = secondRowContainers.map((rowItem) => {
+      return within(rowItem).getByRole("textbox");
+    });
+
+    const keyboardLetterContainerC = getByTestId("keyboardLetterContainer-C");
+    const keyboardLetterContainerH = getByTestId("keyboardLetterContainer-H");
+    const keyboardLetterContainerA = getByTestId("keyboardLetterContainer-A");
+
+    //first input: PEACH
+    //does not matter where the focus is
+    fireEvent.keyDown(firstRowInputs[0], {
+      key: "p",
+    });
+    fireEvent.keyDown(firstRowInputs[0], {
+      key: "e",
+    });
+    fireEvent.keyDown(firstRowInputs[0], {
+      key: "a",
+    });
+    fireEvent.keyDown(firstRowInputs[0], {
+      key: "c",
+    });
+    fireEvent.keyDown(firstRowInputs[0], {
+      key: "h",
+    });
+
+    await waitFor(() => {
+      expect((firstRowInputs[0] as HTMLInputElement).value).toBe("P");
+      expect((firstRowInputs[1] as HTMLInputElement).value).toBe("E");
+      expect((firstRowInputs[2] as HTMLInputElement).value).toBe("A");
+      expect((firstRowInputs[3] as HTMLInputElement).value).toBe("C");
+      expect((firstRowInputs[4] as HTMLInputElement).value).toBe("H");
+    });
+
+    fireEvent.keyDown(firstRowInputs[0], {
+      key: "Enter",
+    });
+
+    await waitFor(() => {
+      //first row
+      expect(firstRowContainers[0].classList.contains("letter-no")).toBe(true);
+      expect(firstRowContainers[1].classList.contains("letter-no")).toBe(true);
+      expect(firstRowContainers[2].classList.contains("letter-correct")).toBe(
+        true
+      );
+      expect(firstRowContainers[3].classList.contains("letter-wrong")).toBe(
+        true
+      );
+      expect(firstRowContainers[4].classList.contains("letter-wrong")).toBe(
+        true
+      );
+
+      //keyboard letters
+      expect(keyboardLetterContainerC.classList.contains("letter-wrong")).toBe(
+        true
+      );
+      expect(keyboardLetterContainerH.classList.contains("letter-wrong")).toBe(
+        true
+      );
+      expect(
+        keyboardLetterContainerA.classList.contains("letter-correct")
+      ).toBe(true);
+    });
+
+    //second input: CHART
+    //does not matter where the focus is
+    fireEvent.keyDown(secondRowInputs[0], {
+      key: "c",
+    });
+    fireEvent.keyDown(secondRowInputs[0], {
+      key: "h",
+    });
+    fireEvent.keyDown(secondRowInputs[0], {
+      key: "a",
+    });
+    fireEvent.keyDown(secondRowInputs[0], {
+      key: "r",
+    });
+    fireEvent.keyDown(secondRowInputs[0], {
+      key: "t",
+    });
+
+    await waitFor(() => {
+      expect((secondRowInputs[0] as HTMLInputElement).value).toBe("C");
+      expect((secondRowInputs[1] as HTMLInputElement).value).toBe("H");
+      expect((secondRowInputs[2] as HTMLInputElement).value).toBe("A");
+      expect((secondRowInputs[3] as HTMLInputElement).value).toBe("R");
+      expect((secondRowInputs[4] as HTMLInputElement).value).toBe("T");
+    });
+
+    fireEvent.keyDown(secondRowInputs[0], {
+      key: "Enter",
+    });
+
+    await waitFor(() => {
+      //first row
+      expect(firstRowContainers[0].classList.contains("letter-no")).toBe(true);
+      expect(firstRowContainers[1].classList.contains("letter-no")).toBe(true);
+      expect(firstRowContainers[2].classList.contains("letter-correct")).toBe(
+        true
+      );
+      expect(firstRowContainers[3].classList.contains("letter-wrong")).toBe(
+        true
+      );
+      expect(firstRowContainers[4].classList.contains("letter-wrong")).toBe(
+        true
+      );
+
+      //second row
+      expect(secondRowContainers[0].classList.contains("letter-correct")).toBe(
+        true
+      );
+      expect(secondRowContainers[1].classList.contains("letter-correct")).toBe(
+        true
+      );
+      expect(secondRowContainers[2].classList.contains("letter-correct")).toBe(
+        true
+      );
+      expect(secondRowContainers[3].classList.contains("letter-no")).toBe(true);
+      expect(secondRowContainers[4].classList.contains("letter-correct")).toBe(
+        true
+      );
+
+      //keyboard letters
+      expect(
+        keyboardLetterContainerC.classList.contains("letter-correct")
+      ).toBe(true);
+      expect(
+        keyboardLetterContainerH.classList.contains("letter-correct")
+      ).toBe(true);
+      expect(
+        keyboardLetterContainerA.classList.contains("letter-correct")
+      ).toBe(true);
+    });
+  });
+
   test("handles double letters in input correctly (input: HHHHH)", async () => {
     const { getAllByTestId, getByTestId } = render(
       <AppContextProvider>
